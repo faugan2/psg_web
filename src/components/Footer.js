435bbe618548c3863ccfa8f8567534789c035917
Lukix01 <e@lklos.com>
2022-04-16 18:39:36 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,23 @@
 import "../styles/footer.scss";
-const Footer=()=>{
-    return(
+const Footer = () => {
+    return (
         <footer className="footer">
             <div className="container">
                 <div>
-                    
-                    <a href="#terms_of_service.html">Terms of Service</a> | 
-                    <a href="#privacy.html">Privacy Statement</a> |
-                    <a href="#faqs.html">FAQs</a> |
-                    <a href="#glossary.html">Glossary</a> |
-                    <a href="#contact.html">Contact Us</a>
-                    
+                    <a href="#terms_of_service.html">Terms of Service</a> |
+                    <a href="#privacy.html"> Privacy Statement</a> |
+                    <a href="#faqs.html"> FAQs</a> |
+                    <a href="#glossary.html"> Glossary</a> |
+                    <a href="#contact.html"> Contact Us</a>
                     <p className="copyright">
-                        Copyright &#169; 2022 PS.G<br />
-                        
+                        Copyright &#169; 2022 PS.G
+                        <br />
                         FOR ENTERTAINMENT/MARKETING PURPOSES ONLY
-                        
                         <div id="debugOutput"></div>
                     </p>
                 </div>
-		    </div>
-		
-    </footer>
-    )
-}
+            </div>
+        </footer>
+    );
+};
 export default Footer;
