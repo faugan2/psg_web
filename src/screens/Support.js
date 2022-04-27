469cf14bc29e22994067d08245e9d310c243e348
Lukix01 <e@lklos.com>
2022-04-27 23:16:49 +0200

    new support page

--- src/screens/Support.js
+++ src/screens/Support.js
@@ -1,47 +1,55 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "../styles/support.scss";
+import image from "../components/img/support.jpeg";
+
+function Box(props) {
+    return (
+        <div className="w-full">
+            <div className="text-xl font-bold mb-3 my-auto">{props.header}</div>
+            <div className="bg-black bg-opacity-20 p-4 rounded-lg">
+                {props.children}
+            </div>
+        </div>
+    );
+}
 
 const Support = () => {
     return (
         <div>
             <Header />
             <div className="container">
-                <div className="support-title">SUPPORT</div>
-                <div className="support-content">
-                We are here to help you with our technical support. If you have any other technical 
-                questions/comments then, we have a special customer support team to help you with all 
-                your questions and get your feedback. ProSport.Gurui is proud to have a dedicated service 
-                team that works for you.<br /><br />
-
-                So, what are you waiting for? Take a deep dive into the wave fantasy sports pick 'em game. 
-                It’s time to test your skills and pick the right player!<br /><br />
-
-                Don’t Miss Out to Witness the Non-Stop Excitement with ProSport.Guru!<br /><br />
-
-                Please mail us at :—----------------<br /><br />
-
-                Contact us at: —------------<br /><br />
-
-
-                    
-                    <div className="support-title">CONTACT US</div>
-                    <div>
-                    Our relationship with our customers is the main aspect of our services.
-                    If you have questions about the games then please email us at —----------.
-                    If you have additional questions, please contact us via email. We would love to respond 
-                    to any inquiries that you could have.<br /><br />
-
-                    We would love to hear from our sports fans- We are all ears. 
-                    Feel free to fling your questions, suggestions, comments, and more. 
-                    Tell us what's great or what we are missing, so we can make sure you are enjoying 
-                    your fullest. Contact us, If you face any difficulties while picking and playing 
-                    with the PS.G app. You have your friendly folks standing by you. We are sitting here 
-                    and starving for feedback, do not forget to give one!<br /><br />
-
-                    For technical support and other questions/comments please go to our support section.
-
+                <div className="grid grid-cols-2">
+                    <div className="grid grid-cols-1">
+                        <Box header="We are here to help you with our technical support">
+                            If you have any other technical questions/comments
+                            then, we have a special customer support team to
+                            help you with all your questions and get your
+                            feedback. ProSport.Gurui is proud to have a
+                            dedicated service team that works for you.
+                        </Box>
+                        <Box header="So, what are you waiting for?">
+                            Take a deep dive into the wave fantasy sports pick
+                            'em game. It’s time to test your skills and pick the
+                            right player!
+                        </Box>
+                        <div className="font-bold space-y-5">
+                            <div>
+                                Don’t Miss Out to Witness the Non-Stop
+                                Excitement with ProSport.Guru!
+                            </div>
+                            <div>
+                                Please mail us at:{" "}
+                                <span className="text-indianred">
+                                    Prosportdotguru@gmail.com
+                                </span>
+                            </div>
+                        </div>
                     </div>
+                    <img
+                        src={image}
+                        alt="img"
+                        className="h-[33rem] m-auto skew-y-3 drop-shadow-xl rounded-lg"
+                    />
                 </div>
             </div>
             <Footer />
